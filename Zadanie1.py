beff829b6a6c36ee170f3a51571999cb66f0373a
Fenrigald <57298298+Fenrigald@users.noreply.github.com>
2021-07-30 17:02:13 +0300

    Update Zadanie1.py

--- Zadanie1.py
+++ Zadanie1.py
@@ -1,3 +1,7 @@
-t = int(input())
-h = t // 360
-print(h)
+Battlecry = 'Ad Mortem!'
+print(Battlecry)
+number = 1654
+print(number)
+anger = input('Show your rage! ')
+print(anger)
+print(int(input('how many lines in this code? ')))
