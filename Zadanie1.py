4c23ecddb8c1164f84b27e2ee35ae9dd9ab00003
Fenrigald <57298298+Fenrigald@users.noreply.github.com>
2021-08-05 14:47:27 +0300

    Complete HW

---
+++ Zadanie1.py
@@ -0,0 +1,3 @@
+ml = [1, 'sword', (2,3,5), False, {'shield': 73, 'soldiers': 67}]
+for i in ml:
+    print(f'{i}, {type(i)}')
---
+++ Zadanie4.py
@@ -0,0 +1,3 @@
+n = input().split()
+for i, n in enumerate(n):
+    print(int(i)+1, n)
